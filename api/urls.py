e9b4e61fbd2528b61ceaef7a5b86e746fc73a71c
galirkil <galirkil@gmail.com>
2024-04-29 21:45:56 +0300

    Update urls

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path, include
+from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView
 from rest_framework.routers import DefaultRouter
 
-from api.views import (ActivateInviteCodeView, ObtainTokensView,
-                       RequestAuthCodeView, UserRetrieveUpdateViewSet)
+from api.views import (ActivateInviteCodeView, CustomRefreshTokenView,
+                       ObtainTokensView, RequestAuthCodeView,
+                       UserRetrieveUpdateViewSet)
 
 app_name = 'api'
 
@@ -14,9 +15,11 @@ urlpatterns = [
     path('request-auth-code/', RequestAuthCodeView.as_view(),
          name='request-auth-code'),
     path('authenticate/', ObtainTokensView.as_view(), name='authenticate'),
-    path('', include(router.urls)),
-    path('users/activate-invite-code/', ActivateInviteCodeView.as_view(),
+    path('refresh-token/', CustomRefreshTokenView.as_view(),
+         name='refresh-token'),
+    path('activate-invite-code/', ActivateInviteCodeView.as_view(),
          name='activate-invite-code'),
+    path('', include(router.urls)),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('schema/redoc/', SpectacularRedocView.as_view(url_name='api:schema'),
          name='redoc')
